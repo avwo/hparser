4f5a2101e8228f1c29a091d908e10877c66be9d5
avwo <avwu@qq.com>
2017-11-21 23:45:23 +0800

    init

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ var util = require('./lib/util');
 
 exports.getRawHeaderNames = util.getRawHeaderNames;
 exports.formatHeaders = util.formatHeaders;
+exports.toMultipart = util.toMultipart;
+exports.toMultiparts = util.toMultiparts;
 exports.parse = require('./lib/parse');
+exports.MultipartParser = require('./lib/multipart-parser');
